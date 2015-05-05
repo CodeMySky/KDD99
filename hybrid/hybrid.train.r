2cc75e89a5c8a71bc34fd6db431a02b7bf4b07d9
CodeMySky <codemysky@hotmail.com>
2015-05-04 21:54:32 -0400

    testing performance on different number of random forest

--- hybrid/hybrid.train.r
+++ hybrid/hybrid.train.r
@@ -16,29 +16,36 @@ feature.selection = c(2,3,4,5,6,12,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,
 #feature.selection = 1:41
 # First layer, use decision tree to classify is.attack
 println('Training normal/abnormal data classifier...')
-dt.model = rpart(is.attack ~ ., data=train.data[,c(feature.selection,43)], method='class')
-if (is.debug == TRUE) {
-  y.hat = predict(dt.model, test.data[,feature.selection], type='vector') - 1
-  print(confusionMatrix(y.hat, as.numeric(test.data[,43])))
+nTree = c(10, 30, 50, 100, 200, 500, 1000, 1500, 2000, 3000, 4000, 5000, 10000)
+for (i in nTree) {
+  random.forest.model = randomForest(as.factor(is.attack) ~ ., data=train.data[c(feature.selection, 43)],
+                                     importance=TRUE, ntree=i)
+  if (is.debug == TRUE) {
+    y.hat = predict(random.forest.model, test.data[,feature.selection])
+    result = confusionMatrix(y.hat, test.data[,43])
+    println(c(i,result$overall[['Accuracy']]))
+  }
 }
+
 
 # Second Layer
-println('Training abnormal data classifier...')
-abnormal.data = data[data$is.attack == TRUE,]
-
-if (is.debug == TRUE) {
-  train.size <- floor(0.3 * nrow(abnormal.data))
-  train.index <- sample(seq_len(nrow(abnormal.data)), size = train.size)
-  train.data <- abnormal.data[train.index, ]
-  test.data <- abnormal.data[-train.index, ]
-} else {
-  train.data = abnormal.data
-}
-
-
-nb.model <- naiveBayes(attack.type ~ ., data = train.data[c(feature.selection, 44)])
-if (is.debug == TRUE) {
-  y.hat = predict(nb.model, test.data[feature.selection])
-  print(confusionMatrix(y.hat, test.data[,44]))
-}
-println('Training finished')
+# println('Training abnormal data classifier...')
+# abnormal.data = data[data$is.attack == TRUE,]
+# 
+# if (is.debug == TRUE) {
+#   train.size <- floor(0.3 * nrow(abnormal.data))
+#   train.index <- sample(seq_len(nrow(abnormal.data)), size = train.size)
+#   train.data <- abnormal.data[train.index, ]
+#   test.data <- abnormal.data[-train.index, ]
+# } else {
+#   train.data = abnormal.data
+# }
+# 
+# 
+# nb.model <- naiveBayes(attack.type ~ ., data = train.data[c(feature.selection, 44)])
+# random.forest.model = randomForest(attack.type ~ ., data=train.data[c(feature.selection, 44)], importance=TRUE, ntree=2000)
+# if (is.debug == TRUE) {
+#   y.hat = predict(nb.model, test.data[feature.selection])
+#   print(confusionMatrix(y.hat, test.data[,44]))
+# }
+# println('Training finished')
