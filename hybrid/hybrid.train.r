444d9a058afb71a13961e0f32546058ed8e56a75
CodeMySky <codemysky@hotmail.com>
2015-05-10 23:46:42 -0400

    + multi class classification

--- hybrid/hybrid.train.r
+++ hybrid/hybrid.train.r
@@ -3,7 +3,7 @@ set.seed(1)
 is.debug = TRUE
 # Prepare, split train and test
 if (is.debug == TRUE)  {
-  train.size <- floor(0.8 * nrow(data))
+  train.size <- floor(0.7 * nrow(data))
   train.index <- sample(seq_len(nrow(data)), size = train.size)
   train.data <- data[train.index, ]
   test.data <- data[-train.index, ]
@@ -12,8 +12,8 @@ if (is.debug == TRUE)  {
   train.data = data
 }
 
-feature.selection = c(2,3,4,5,6,12,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41)
-#feature.selection = 1:41
+#feature.selection = c(2,3,4,5,6,12,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41)
+feature.selection = 1:41
 # First layer, use decision tree to classify is.attack
 println('Training normal/abnormal data classifier...')
 ## Scale does not help to improve accuracy
@@ -21,23 +21,41 @@ println('Training normal/abnormal data classifier...')
 # real.test.data[1:41] = scale(data.matrix(real.test.data[1:41]))
 # test.data[1:41] = scale(data.matrix(test.data[1:41]))
 
-train.normal = train.data[train.data$is.attack == FALSE, feature.selection]
-train.attack = train.data[train.data$is.attack == TRUE, feature.selection]
-train.normal = data.matrix(train.normal)
-train.attack = data.matrix(train.attack)
+train.dos = train.data[train.data$attack.type=='dos', feature.selection]
+train.normal = train.data[train.data$attack.type=='normal', feature.selection]
+train.probe = train.data[train.data$attack.type=='probe', feature.selection]
+train.r2l = train.data[train.data$attack.type=='r2l', feature.selection]
+train.u2r = train.data[train.data$attack.type=='u2r', feature.selection]
 
-for (i in c(2,3,4,5,6,7,8,9,10,15,20,50,100,200,500,1000, 1500, 2000)) {
-  normal.centers = kmeans(train.normal, centers = i,iter.max = 10)$centers
-  attack.centers = kmeans(train.attack, centers = 3 * i,iter.max = 10)$centers
-  y.labels = c(rep(FALSE,dim(normal.centers)[1]), rep(TRUE,dim(attack.centers)[1]))
-  x.centers = rbind(normal.centers, attack.centers)
+train.dos = data.matrix(train.dos)
+train.normal = data.matrix(train.normal)
+train.probe = data.matrix(train.probe)
+train.r2l = data.matrix(train.r2l)
+train.u2r = data.matrix(train.u2r)
+test.data = test.data[test.data$label != 'normal',]
+real.test.data = real.test.data[real.test.data$attack.type != 'normal',]
+
+
+for (i in c(5)) {
+  
+  dos.centers = kmeans(train.dos, centers = i*6 ,iter.max = 10)$centers
+#   normal.centers = kmeans(train.normal, centers = i*3,iter.max = 10)$centers
+  probe.centers = kmeans(train.probe, centers = i,iter.max = 10)$centers
+  r2l.centers = kmeans(train.r2l, centers = i,iter.max = 10)$centers
+  u2r.centers = kmeans(train.u2r, centers = i,iter.max = 10)$centers
+  y.labels = c(rep('dos',dim(dos.centers)[1]),
+#                 rep('normal',dim(normal.centers)[1]),
+                   rep('probe',dim(probe.centers)[1]),
+                       rep('r2l',dim(r2l.centers)[1]),
+                           rep('u2r',dim(u2r.centers)[1]))
+  x.centers = rbind(dos.centers,  probe.centers, r2l.centers, u2r.centers)
   
   
   y.hat = knn(x.centers, data.matrix(test.data[,feature.selection]), y.labels, k=1)
-  result1 = confusionMatrix(y.hat, test.data$is.attack)
+  result1 = confusionMatrix(as.character(y.hat), as.character(test.data$attack.type))
   
   y.hat = knn(x.centers, data.matrix(real.test.data[,feature.selection]), y.labels, k=1)
-  result2 = confusionMatrix(y.hat, real.test.data$is.attack)
+  result2 = confusionMatrix(as.character(y.hat), as.character(real.test.data$attack.type))
   print(c(i, result1$overall[['Accuracy']], result2$overall[['Accuracy']]))
 }
 
