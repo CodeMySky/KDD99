a30b296f642d21495f1888bb0149014c3672f829
CodeMySky <codemysky@hotmail.com>
2015-05-10 23:39:18 -0400

    + accuracy

--- ood/hybrid.train.test.r
+++ ood/hybrid.train.test.r
@@ -6,17 +6,17 @@ hybrid.train.test <- function(data.set) {
   test.data = data.set[['testing']]
   # First layer, use decision tree to classify is.attack
   println('Training normal/abnormal data classifier...')
-  type = 'random.forest'
+  type = 'decision.tree'
   binary.classifier <-
     switch(type,
            decision.tree = {
-             # Train a decision tree
+             # Train a decision tree, Accuracy : 0.925  
              binary.classifier = rpart(is.attack ~ ., data=train.data[,c(feature.selection,43)])
              y.hat = predict(binary.classifier, test.data[,feature.selection])
              binary.result = confusionMatrix(round(y.hat), as.numeric(test.data$is.attack))
            },
            random.forest = {
-             # Train a random forest
+             # Train a random forest，Accuracy : 0.9258 
              train.data.matrix  <-  train.data
              train.data.matrix[1:41] <- data.matrix(train.data[1:41])
              train.data.matrix$is.attack <- as.factor(train.data.matrix$is.attack)
