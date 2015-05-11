921add572110c2f1eb27d46a88ab3a37de277544
CodeMySky <codemysky@hotmail.com>
2015-05-11 10:51:08 -0400

    + means to first layer

--- ood/hybrid.train.test.r
+++ ood/hybrid.train.test.r
@@ -1,14 +1,14 @@
 hybrid.train.test <- function(data.set) {
-  #feature.selection = c(2,3,4,5,6,12,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41)
+  special.feature.selection = c(2,3,4,5,6,12,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41)
   feature.selection = 1:41
   
   train.data = data.set[['training']]
   test.data = data.set[['testing']]
   # First layer, use decision tree to classify is.attack
   println('Training normal/abnormal data classifier...')
-  type = 'decision.tree'
+  binary.type = 'kmeans'
   binary.classifier <-
-    switch(type,
+    switch(binary.type,
            decision.tree = {
              # Train a decision tree, Accuracy : 0.925  
              binary.classifier = rpart(is.attack ~ ., data=train.data[,c(feature.selection,43)])
@@ -27,7 +27,27 @@ hybrid.train.test <- function(data.set) {
                                               importance=TRUE, ntree=10, type="class")
              y.hat = predict(binary.classifier, test.data.matrix[,feature.selection], type="class")
              binary.result = confusionMatrix(y.hat, test.data$is.attack)
+           },
+           kmeans = {
+             # Create template and use KNN to classify data, Accuracy : 0.9228
+             # Accuracy can be improved by adjusting number of centers!
+             train.normal = data.matrix(train.data[train.data$label == 'normal', 
+                                                   special.feature.selection])
+             train.attack = data.matrix(train.data[train.data$label != 'normal', 
+                                                   special.feature.selection])
+             normal.centers = kmeans(train.normal, centers = 100,iter.max = 10)$centers
+             attack.centers = kmeans(train.attack, centers = 300,iter.max = 10)$centers
+             x.centers = rbind(normal.centers,  attack.centers)
+             y.labels = c(rep(FALSE,dim(normal.centers)[1]),
+                          rep(TRUE,dim(attack.centers)[1]))
+             y.hat = knn(x.centers, data.matrix(test.data[,special.feature.selection]), y.labels, k=1)
+             binary.result = confusionMatrix(y.hat, test.data$is.attack)
            })
-  print(type)
+  print(binary.type)
   print(binary.result)
+  
+  
+  
+  # Begin multi-class classification
+  
 }
